6d51b422cf8c17ac06361803a8e544081c432c54
mgh-gadzOOks <quantumlazertigerdragon@gmail.com>
2022-04-05 19:30:31 -0500

    organized files, created beginning character folder and files.

---
+++ GadzOOks/GadzOOks/Diction/DialogRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GadzOOks
+{
+    internal class DR
+    {
+        static public String menuInstructions = "Type one of the following single character options\nand press enter to select a menu option:";
+        static public String[] mainMenuOptions = { "(S)tart Game", "(O)ptions", "(Q)uit" };
+        static public String[] mainMenuChoices = { "s", "o", "q" };
+        static public String[] optionsMenuOptions = { "(T)ext speed change", "(B)ack to main menu" };
+        static public String[] optionsMenuChoices = { "t", "b"};
+        static public String[] textSpeedMenuOptions = { "(0) INSTANT", "(1) FAST", "(2) NORMAL", "(3) SLOW" };
+        static public String[] textSpeedMenuChoices = { "0", "1", "2", "3" };
+    }
+}
